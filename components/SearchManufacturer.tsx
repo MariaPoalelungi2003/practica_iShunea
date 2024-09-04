274bd249df27cecf1807ce0171df1771e97b2596
MariaPoalelungi2003 <poalelungimaria978@gmail.com>
2024-09-04 23:29:19 +0300

    first commit

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -5,6 +5,7 @@ import { Combobox, Transition } from "@headlessui/react";
 import {SearchManufacturerProps} from "@/types";
 import Image from "next/image";
 
+
 const SearchManufacturer = ({ manufacturer, setManufacturer}: SearchManufacturerProps) => {
     
     const [query, setQuery] = useState('')
