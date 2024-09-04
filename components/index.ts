7a2fd336f9cb39225940875d4dfdf7e2a6bbec93
MariaPoalelungi2003 <poalelungimaria978@gmail.com>
2024-09-04 23:22:06 +0300

    SearchManufacturer

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import Hero from "@/components/Hero";
+import CustomButton from "@/components/CustomButton";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import CustomFilter from "@/components/CustomFilter";
+import SearchBar from "@/components/SearchBar";
+import SearchManufacturer from "@/components/SearchManufacturer";
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
+}
